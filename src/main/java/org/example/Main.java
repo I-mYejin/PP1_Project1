5de9b5913bb209c20dbba53a6299c492b3813d03
I-mYejin <iamyejin0318@gmail.com>
2023-09-07 23:42:14 +0900

    [Update] 간격/문장 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("*** 영단어 마스터 ***\n");
+        System.out.println("*** 영단어 마스터 ***");
         new WordManager().start();
     }
 
